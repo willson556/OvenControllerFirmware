abd64f7dbf304d7d303c45d282fa11360fe40028
Thomas Willson <thomas.willson@me.com>
2019-05-04 19:20:39 -0700

    Improve unit conversion accuracy.

--- main/units.hpp
+++ main/units.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <math.h>
+
 constexpr int CelsiusToFahrenheit(int c) {
-    return c * 9 / 5 + 32;
+    return round(c * 9.0 / 5.0 + 32);
 }
 
 constexpr int FahrenheitToCelsius(int f) {
-    return (f - 32) * 5 / 9;
+    return round((f - 32) * 5.0 / 9.0);
 }
